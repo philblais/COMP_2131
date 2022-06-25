27f7ac993c6df0974f1c8574d7a3ccfba795108d
phib <phil@blais.dev>
2022-06-24 18:55:13 -0700

    working on game pages, some javascript and sass

--- scripts/final-assignment.js
+++ scripts/final-assignment.js
@@ -1,64 +1,35 @@
 // constants
+const gameStartView = document.getElementById('game-start');
+const gameGridView = document.getElementById('game-grid');
 const btnStartAnimation = document.getElementById('btn-start-animation');
-const btnStopAnimation = document.getElementById('btn-stop-animation');
-const bikeImageInHTML = document.getElementById('bike');
-const popup = document.getElementById('pop-up');
-const closeButton = document.getElementById('close-pop-up');
-const contentWrapper = document.getElementById('content-wrap');
-const bikeAnimationDelay = 75;
-const popupDelay = 3000;
+const btnEndAnimation = document.getElementById('btn-end-animation');
+const diceImageInHTML = document.getElementById('dice');
+const diceAnimation = document.getElementById('dice-animation');
 
-// variables
-let bikeAnimation;
-let position = 0;
-let isAnimating = false;
-let timeoutHandler;
-let popupTimeoutHandler;
+requestAnimationFrame( diceRolling );
+
+function diceRolling(){
+
+    // let children = diceAnimation.children;
+    // for (let i = 0; i < children.length; i++) {
+    //     let tableChild = children[i];
+    //     let position = tableChild.style.left
+    //     for (let j = position; j < 50; j++) {
+    //         setInterval(function(){
+    //             console.log(tableChild.style.left)
+    //             tableChild.style.left = `${j}%`
+    //         }, 1000);
+    //     }
+    // }
+}
 
 // listeners
 btnStartAnimation.addEventListener('click', function () {
-    clearTimeout(popupTimeoutHandler);
-    if (!isAnimating) {
-        isAnimating = true;
-        bikeAnimation = requestAnimationFrame(rotate);
-    }
+    gameStartView.style.setProperty("display", "none")
+    gameGridView.style.setProperty("display", "grid")
 });
 
-btnStopAnimation.addEventListener('click', function () {
-    cancelAnimationFrame(bikeAnimation);
-    clearTimeout(timeoutHandler);
-    isAnimating = false;
+btnEndAnimation.addEventListener('click', function () {
+    gameStartView.style.setProperty("display", "grid")
+    gameGridView.style.setProperty("display", "none")
 });
-
-closeButton.addEventListener('click', function () {
-    closePopUp()
-});
-
-// functions
-
-popupTimeoutHandler = setTimeout(function () {
-    popup.classList.remove("hidden");
-    popup.classList.add("visible");
-    popup.style.setProperty('z-index', '1')
-    contentWrapper.style.setProperty('z-index', '-1')
-}, popupDelay);
-
-function rotate() {
-    timeoutHandler = setTimeout(function () {
-            if (position < 34) {
-                position++;
-            } else {
-                position = 1;
-            }
-            bikeImageInHTML.src = `images/bike-${position}.jpg`
-            bikeAnimation = requestAnimationFrame(rotate);
-        }, bikeAnimationDelay);
-}
-
-function closePopUp() {
-    clearTimeout(popupTimeoutHandler);
-    popup.classList.remove("visible");
-    popup.classList.add("hidden");
-    popup.style.setProperty('z-index', '-1');
-    contentWrapper.style.setProperty('z-index', '1');
-}
